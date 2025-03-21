3a8effbeab18bcce01099b490e3157ace878575f
Neyrrrs <neyrrrs@gmail.com>
2025-03-21 20:45:04 +0700

    FU: Adding auth form and usercontroller

---
+++ userSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leaernify
+{
+    internal class userSession
+    {
+        public static string username { get; set; }
+        public static string password { get; set; }
+        public static int id { get; set; }
+
+        public static void logout() {
+            username = null;
+            username = null;
+            id = 0;
+        }
+    }
+}
